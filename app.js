8d80c185c640d468497b6490ef862ef51296cb0c
James Perrin <countrydawgg@gmail.com>
2021-02-24 18:25:46 -0800

    Add Express Routing

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.error(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepate');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
